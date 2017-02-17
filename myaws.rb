22e4cb3c172e168f0ab06e7ff2c398f9613e7b52
Masayuki Morita <masayuki.morita@crowdworks.co.jp>
2017-02-17 14:35:25 +0900

    Update to v0.0.9

--- myaws.rb
+++ myaws.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class Myaws < Formula
   homepage "https://github.com/minamijoyo/myaws"
-  url "https://github.com/minamijoyo/myaws/releases/download/v0.0.8/myaws_v0.0.8_darwin_amd64.tar.gz"
-  sha256 "fc8f8d2638323f76f251a9228197caad2f6f38fb744c8f9acac1b01d58b25d61"
+  url "https://github.com/minamijoyo/myaws/releases/download/v0.0.9/myaws_v0.0.9_darwin_amd64.tar.gz"
+  sha256 "5887b482eb6f2368a42548f6b413ff3ae9fecc39ffaca37b7ad1e740d653e27e"
   head "https://github.com/minamijoyo/myaws.git"
-  version "0.0.8"
+  version "0.0.9"
 
   def install
     bin.install "myaws"
