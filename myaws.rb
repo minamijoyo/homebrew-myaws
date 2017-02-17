a32d0f725c52ada38ad1d24911ef3e93c2c4cc70
Masayuki Morita <masayuki.morita@crowdworks.co.jp>
2017-02-17 14:39:35 +0900

    Fix sha256 for v0.0.9

--- myaws.rb
+++ myaws.rb
@@ -3,7 +3,7 @@ require "formula"
 class Myaws < Formula
   homepage "https://github.com/minamijoyo/myaws"
   url "https://github.com/minamijoyo/myaws/releases/download/v0.0.9/myaws_v0.0.9_darwin_amd64.tar.gz"
-  sha256 "5887b482eb6f2368a42548f6b413ff3ae9fecc39ffaca37b7ad1e740d653e27e"
+  sha256 "6b809131f219e2d2d08581d1a9e1d64257aebffe41846a8957927704b71fae04"
   head "https://github.com/minamijoyo/myaws.git"
   version "0.0.9"
 
