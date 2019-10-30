b73e59843ad61ad2b957ab69c4a8474c7274d83d
Masayuki Morita <minamijoyo@gmail.com>
2019-10-30 16:54:33 +0900

    Brew formula update for myaws version v0.3.11

--- myaws.rb
+++ myaws.rb
@@ -2,9 +2,9 @@
 class Myaws < Formula
   desc "A human friendly AWS CLI written in Go"
   homepage "https://github.com/minamijoyo/myaws"
-  url "https://github.com/minamijoyo/myaws/releases/download/v0.3.10/myaws_v0.3.10_darwin_amd64.tar.gz"
-  version "0.3.10"
-  sha256 "a2144f470335f22bdd09774f1cbaada61c282a6441c0d3783fdefcc3a0930a74"
+  url "https://github.com/minamijoyo/myaws/releases/download/v0.3.11/myaws_v0.3.11_darwin_amd64.tar.gz"
+  version "0.3.11"
+  sha256 "1ed31b71e918ee30f8a5a42bb947b8b97202fdbe4a20eaa5041cf3fd007b0bfa"
 
   def install
     bin.install "myaws"
